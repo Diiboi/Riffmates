007936f794593d635eda7d95098c7204d51375ed
Dieudonne NIYONKURU <118180436+Diiboi@users.noreply.github.com>
2025-05-14 14:11:16 +0800

    make some changes

--- bands/models.py
+++ bands/models.py
@@ -4,8 +4,8 @@ class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     birth=models.DateField()
-def __str__(self):
-    return f"Musiciane(id={self.id}, first_name={self.first_name}, last_name={self.last_name}, birth={self.birth})"
+    def __str__(self):
+     return f"Musiciane(id={self.id}, first_name={self.first_name}, last_name={self.last_name}, birth={self.birth})"
 
 class Venue(models.Model):
     name = models.CharField(max_length=50)
